50a5fd7e6988130b7098fc3a0ed980218ea95125
UC0025 <yushi.toyoda@di-v.co.jp>
2020-03-09 17:43:18 +0900

    自動更新完成

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -65,25 +65,29 @@ $(function(){
    });
   })
   var reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
     var last_message_id = $('.message:last').data("message-id");
-    console.log(last_message_id);
     $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
       url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
       type: 'get',
       dataType: 'json',
-      //dataオプションでリクエストに値を含める
       data: {id: last_message_id}
     })
     .done(function(messages) {
-      console.log('success');
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main-chat__space__messages').append(insertHTML);
+        $('.main-chat__space').animate({ scrollTop: $('.main-chat__space__messages')[0].scrollHeight});
+      }
     })
     .fail(function() {
       alert('error');
     });
   };
-
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
 
